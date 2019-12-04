8b2c1a34fc1beb652fe28a4b3023d89ceff0a5f0
amitjslearn <amitjslearn@gmail.com>
2019-12-04 21:24:20 +0530

    try - lets see what happens

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 
 from ocr_core import ocr_date
 
+pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
 
 UPLOAD_FOLDER = '/static/uploads/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
