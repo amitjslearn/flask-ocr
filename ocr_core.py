596b1809a42ac6c4eeb1a42a1f86c7d961b8f8b8
amitjslearn <amitjslearn@gmail.com>
2019-12-04 23:53:53 +0530

    pytess before calling it

--- ocr_core.py
+++ ocr_core.py
@@ -7,11 +7,12 @@ import regex as rx
 import validation as vd
 import pars as pr
 # pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
-pytesseract.pytesseract.tesseract_cmd ='/.apt/usr/share/tesseract-ocr/4.00/tessdata'
+# pytesseract.pytesseract.tesseract_cmd ='/.apt/usr/share/tesseract-ocr/4.00/tessdata'
 def ocr_core(filename):
     """
     This function will handle the core OCR processing of images.
     """
+    pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
     text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
     return text
 
