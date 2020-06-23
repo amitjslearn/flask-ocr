f05324fb1b1c342a8d12f7133c4de508cf32bf20
amitjslearn <amitjslearn@gmail.com>
2020-06-23 14:53:20 +0530

    bash file to instsall requirements

---
+++ install_requirements.sh
@@ -0,0 +1,10 @@
+sudo apt-get -y update;
+sudo apt-get -y install libleptonica-dev;
+sudo apt-get -y install tesseract-ocr tesseract-ocr-dev;
+sudo apt-get -y install libtesseract-dev;
+sudo snap install --channel=edge tesseract;
+sudo apt-get -y install tesseract-ocr-eng
+pip install tesseract;
+pip install tesseract-ocr;
+pip install pytesseract;
+pip install python-dateutil==2.8.0;
